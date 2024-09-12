5c45cd1c11e9cb9ce76f833b575e7d582124df9f
erandiiii <erandberisha1@gmail.com>
2024-09-12 15:53:38 +0200

    asd

--- src/components/aboutus/AboustUs.jsx
+++ src/components/aboutus/AboustUs.jsx
@@ -9,7 +9,8 @@ import { IoIosArrowRoundForward } from 'react-icons/io';
 
 const AboustUs = () => {
     const breakPoints = [
-        { width: 1, itemsToShow: 2 },
+        
+        { width: 1, itemsToShow: 1 },
         { width: 550, itemsToShow: 2 },
         { width: 768, itemsToShow: 2 },
         { width: 1200, itemsToShow: 2 },
