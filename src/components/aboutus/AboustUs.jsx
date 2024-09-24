ba85a79d63d97b94ea0b174aab5027295eb79ef9
erandiiii <erandberisha1@gmail.com>
2024-09-24 14:10:23 +0200

    asd

--- src/components/aboutus/AboustUs.jsx
+++ src/components/aboutus/AboustUs.jsx
@@ -16,8 +16,8 @@ const AboustUs = () => {
     const breakPoints = [
 
         { width: 1, itemsToShow: 1 },
-        { width: 550, itemsToShow: 2 },
-        { width: 768, itemsToShow: 2 },
+        { width: 750, itemsToShow: 1 },
+        { width: 868, itemsToShow: 2 },
         { width: 1200, itemsToShow: 2 },
     ];
 
